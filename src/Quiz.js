a0f348ff130191da1cf0f4ef64e9542d062dec91
Rupesh Jain <rjain@empoweredbenefits.com>
2020-05-23 14:28:59 +0530

    REset the quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -18,6 +18,12 @@ class Quiz extends Component {
     this.setState({ quiz_position: this.state.quiz_position + 1 });
   }
 
+  handleResetClick() {
+    this.setState({
+      quiz_position: 1
+    });
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -25,7 +31,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             showNextQuestionHandler={this.showNextQuestion.bind(this)}
